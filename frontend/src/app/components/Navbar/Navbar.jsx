c83ea020a4e9f2b6cb364f0b7ae139fc2b1e2e69
Cole Plagens <142531035+Colep39@users.noreply.github.com>
2025-10-18 14:21:56 -0500

    set up navbar and pages files setup

---
+++ frontend/src/app/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Navbar(){
+
+  return(
+    <>
+        <nav className="w-full flex items-center justify-between py-4 px-8 bg-white shadow-md">
+            <div className="text-2xl font-bold text-gray-600">Hacking The Climate</div>
+            <div>
+                <a href="#" className="text-gray-600 hover:text-gray-800 mx-4">Home</a>
+                <a href="#" className="text-gray-600 hover:text-gray-800 mx-4">About</a>
+                <a href="#" className="text-gray-600 hover:text-gray-800 mx-4">Admin Panel</a>
+                <a href="#" className="text-gray-600 hover:text-gray-800 mx-4">Reviews</a>
+            </div>
+        </nav>
+    </>
+  );
+}
