a1199571c682e621b451bab4ee0671c63d8ce204
Cole Plagens <142531035+Colep39@users.noreply.github.com>
2025-10-18 17:26:16 -0500

    dashboard, reviews, about pages

--- frontend/src/app/components/Navbar/Navbar.jsx
+++ frontend/src/app/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar(){
         <nav className="w-full flex items-center justify-between py-4 px-8 bg-white shadow-md">
             <div className="text-2xl font-bold text-gray-600">Hacking The Climate</div>
             <div>
-                <a href="/" className="text-gray-600 hover:text-gray-800 mx-4">Home</a>
+                <a href="/" className="text-gray-600 hover:text-gray-800 mx-4">Dashboard</a>
                 <a href="/about" className="text-gray-600 hover:text-gray-800 mx-4">About</a>
                 <a href="/adminPanel" className="text-gray-600 hover:text-gray-800 mx-4">Admin Panel</a>
                 <a href="/reviews" className="text-gray-600 hover:text-gray-800 mx-4">Reviews</a>
